e5fca99cb0abed4c7f4cf186440055dba9c38c82
Arthur Yukio Hayashi <arthuryukiohayashi@gamil.com>
2023-10-17 15:36:48 -0300

    curso-laravel

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+
+Route::get('/', function () {
+
+    return view('menu',[
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('post/{post}', function ($slug) {
+
+    return view('primeiroPost',[
+        'post' => Post::findOrFail($slug)
+        
+    ]);
+
+});
