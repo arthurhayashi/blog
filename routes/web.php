3a638f5aeaa6b057c1288fb44cb0e4eb936af5a4
Arthur Yukio Hayashi <arthuryukiohayashi@gamil.com>
2023-10-19 15:09:15 -0300

    Forms-Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -22,3 +24,9 @@ Route::get('post/{post:slug}', [PostController::class, 'show']);
 //     ]);
 // })->name('category');
 
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
