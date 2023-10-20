9b3347eb081cbe56f956b81a46962c12d0e0b11c
Arthur Yukio Hayashi <arthuryukiohayashi@gamil.com>
2023-10-20 11:00:49 -0300

    Newsletters-APIs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -32,3 +33,5 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('newsletter', NewsletterController::class);
